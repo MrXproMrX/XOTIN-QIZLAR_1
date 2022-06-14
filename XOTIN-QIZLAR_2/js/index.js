9fce027a490d529438d4c4698cb87b069807b119
marat <mrxpro2003@gmail.com>
2022-06-14 11:45:08 +0500

    хаммаси узгартрилди

--- XOTIN-QIZLAR_2/js/index.js
+++ XOTIN-QIZLAR_2/js/index.js
@@ -73,3 +73,19 @@ $(document).ready(function(){
   })
 });
 });
+
+
+
+$(document).ready(function(){
+  $('.header__from__true').click(function(){
+    $('.header__form').fadeTo(500, 1)
+    $('.header__form').css('z-index','555').css('opacity','1').css('margin','0')
+  })
+});
+
+$(document).ready(function(){
+  $('.header__button__false').click(function(){
+      $('.header__form').fadeTo(500, 1)
+      $('.header__form').css('z-index','0').css('opacity','0').css('margin','-100%')
+  })
+});
